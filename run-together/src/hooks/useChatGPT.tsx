98297088b6baade6782abd874a059923d7087ed9
Patrick Yang <patrick.yang@goanimate.com>
2023-03-17 23:56:29 +0800

    add GPT prompt type
    Change-Id: I7211b43328c9c95ba3231e301157d0936920e499

--- run-together/src/hooks/useChatGPT.tsx
+++ run-together/src/hooks/useChatGPT.tsx
@@ -8,7 +8,7 @@ import { sideBarChatroomListActions } from "../redux/sidebarChatroomList/slice";
 
 const useChatGPT = (currentParticipant: IUser) => {
   const dispatch = useAppDispatch();
-  const [prompt, setPrompt] = useState("");
+  const [prompt, setPrompt] = useState<string>("");
   const [history, setHistory] = useState<string[]>([
     "The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?",
   ]);
