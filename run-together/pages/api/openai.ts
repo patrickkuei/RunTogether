faa47eca372c017b8f2971bcd25a910d378949f7
Patrick Yang <patrick.yang@goanimate.com>
2023-03-18 00:37:54 +0800

    modify environment variable
    Change-Id: Ie5e05697f0c955de80544a220f0b63b6ed057e7b

--- run-together/pages/api/openai.ts
+++ run-together/pages/api/openai.ts
@@ -2,24 +2,30 @@ import { NextApiResponse, NextApiRequest } from "next";
 import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
-  apiKey: process.env.ENV_LOCAL_OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY,
 });
 
 const openai = new OpenAIApi(configuration);
 
 export default async function handler(
   _req: NextApiRequest,
-  res: NextApiResponse<any>
+  res: NextApiResponse<string>
 ) {
-  const response = await openai.createCompletion({
-    model: "text-davinci-003",
-    prompt: _req.body.prompt,
-    temperature: 0.6,
-    max_tokens: 1500,
-    top_p: 1,
-    frequency_penalty: 0,
-    presence_penalty: 0.6,
-  });
+  try {
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: _req.body.prompt,
+      temperature: 0.6,
+      max_tokens: 1500,
+      top_p: 1,
+      frequency_penalty: 0,
+      presence_penalty: 0.6,
+    });
 
-  return res.status(200).json(response.data.choices[0].text || "");
+    return res.status(200).json(response.data.choices[0].text || "");
+  } catch (error) {
+    console.error(error);
+
+    return res.status(200).json("Server Error Occured");
+  }
 }
